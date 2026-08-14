6b65579b7e4319e15cbba7a4f78691385bb30c68
Jnoks <joel@jnoks.dev>
2026-08-14 17:19:21 +0200

    Damage is reduced by 50% if a shield is in INV

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const spaceship = "Titan";
 let shipHealth = 100;
 let credits = 1000;
-const inventory = ["repairKit", "repairKit", "repairKit"];
+const inventory = ["shield", "repairKit", "repairKit", "repairKit"];
 
 function renderStatus() {
   document.getElementById("stats").innerHTML = /*html*/ `
@@ -35,11 +35,12 @@ function renderStatus() {
 }
 
 function showMessage(text) {
-  document.getElementById("message").innerHTML = '<span class="prompt">> </span>' + text;
+  document.getElementById("message").innerHTML =
+    '<span class="prompt">> </span>' + text;
 }
 
 function useRepairKit() {
-  let index = inventory.indexOf("repairKit")
+  const index = inventory.indexOf("repairKit");
   if (index == -1) {
     showMessage("No repair kits left.");
     renderStatus();
@@ -64,7 +65,7 @@ function buyRepairKit() {
   if (amount <= 0) {
     showMessage("Please enter a valid amount.");
     renderStatus();
-    return; 
+    return;
   }
   const totalCost = amount * 100;
   if (credits >= totalCost) {
@@ -84,7 +85,7 @@ function buyShield() {
   if (amount <= 0) {
     showMessage("Please enter a valid amount.");
     renderStatus();
-    return; 
+    return;
   }
   const totalCost = amount * 50;
   if (credits >= totalCost) {
@@ -100,7 +101,7 @@ function buyShield() {
 }
 
 function takeDamage() {
-  const damage = Number(document.getElementById("damage-input").value);
+  let damage = Number(document.getElementById("damage-input").value);
   if (damage <= 0) {
     showMessage("Please enter a valid amount.");
     renderStatus();
@@ -111,14 +112,24 @@ function takeDamage() {
     renderStatus();
     return;
   }
+  const index = inventory.indexOf("shield");
+  if (index !== -1) {
+    inventory.splice(index, 1);
+    damage = damage / 2;
+    showMessage(
+      "You took " +
+        damage +
+        " damage. Damage was reduced by 50% due to a shield",
+    );
+  } else {
+    showMessage("You took " + damage + " damage.");
+  }
   shipHealth = shipHealth - damage;
   if (shipHealth < 0) {
     shipHealth = 0;
   }
   if (shipHealth === 0) {
     showMessage("Your ship was destroyed. Game over!");
-  } else {
-    showMessage("You took " + damage + " damage.");
   }
   renderStatus();
 }
